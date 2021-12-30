4237a7c1b8e7d2950747083592df612ccf329d6f
MatMasIt <mattia.mascarello@outlook.it>
2021-12-30 03:09:05 +0100

    Update age.php

--- age.php
+++ age.php
@@ -1,13 +1,14 @@
 <?php
 $a=file("README.md");
+$final ="";
 foreach($a as $line){
-echo $a;
    $q=explode("<!--ageB->",$line);
-   echo $q[0];
+   $final.=$q[0];
    if(count($q)==2){
      $dateOfBirth = "19-11-2002";
      $today = date("Y-m-d");
      $diff = date_diff(date_create($dateOfBirth), date_create($today));
-     echo '<!--ageB->'.$diff->format('%y')."|\n";
+     $final.= '<!--ageB->'.$diff->format('%y')."|\n";
    }
 }
+file_put_contents("README.md",$final);
