0b5a25a9fe2e77d168c939a79d7353c2573306df
MatMasIt <mattia.mascarello@outlook.it>
2021-12-30 02:52:24 +0100

    Create age.php

---
+++ age.php
@@ -0,0 +1,12 @@
+<?php
+$a=file("README.md");
+foreach($a as $line){
+   $q=explode("<!--ageB->",$line);
+   echo $q[0];
+   if(count($q)==2){
+     $dateOfBirth = "19-11-2002";
+     $today = date("Y-m-d");
+     $diff = date_diff(date_create($dateOfBirth), date_create($today));
+     echo '<!--ageB->'.$diff->format('%y')."|\n";
+   }
+}
